57284e6a284fe32d46ef5ffe0915b1a85454841c
Lena Hou <lenahou2013@gmail.com>
2021-01-28 19:08:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ my_array = ["cats", "dogs", "rabbits", "fish"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
